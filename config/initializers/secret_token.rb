66ee449d4f132dfefbc5a4154bc2e6b11a1cd57b
Daniel Smith <jellymann@gmail.com>
2014-03-07 10:53:10 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsThing::Application.config.secret_key_base = '364bbc3567ee40872f4436887352cd2cd8b871a0e1f924688fc68b31684132a17d0c043ecd6c52a83b94e44103b38036e2f9dcf6c49d79af1cc46549369e49b0'
